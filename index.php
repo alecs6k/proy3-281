f47c1659f705653dc3ec0ae2839e11709869d59a
alecs6k <alecsreto@gmail.com>
2021-11-26 00:03:40 -0400

    favicon

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- Favicon -->
-	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<link rel="shortcut icon" type="image/x-icon" href="favicon2.ico" />
 
 	<!-- CSS
 		================================================== -->
